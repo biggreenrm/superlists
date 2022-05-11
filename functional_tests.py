a4f1f4493cb7592e456ae9686c9d87b74e24f11f
Сергей Гурков <sergeygurkov@MacBook-Pro-Sergej.local>
2022-05-11 10:15:45 +0300

    [DEV] main: functional test now checks ability to enter list's element

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,21 +20,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # She see that title tells her it is To-Do list
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish test")
+        header_text: str = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is immediately prompted to enter a list item
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types in the text box "Buy peacock feathers"
         # (her hobby is knitting fishing flies)
-
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she presses enter, the page refreshes and now
         # the page contains "1: Buy Peacock Feathers" as a list item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # the text field still invites her to add another element.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # The text field still invites her to add another element.
         # She introduces "Make a fly out of peacock feathers"
         # (Amber is very methodical)
+        self.fail('End test!')
 
 
         # The page is updated again, and now shows both elements of its list
