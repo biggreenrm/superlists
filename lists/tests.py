8b00fe89c08583fe4536180208ef3bf64419020a
Сергей Гурков <sergeygurkov@MacBook-Pro-Sergej.local>
2022-05-11 09:33:51 +0300

    [DEV] main: home page view returns correct html

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,24 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
     """Test for toxicity."""
 
-    def test_bad_math(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """test: root url resolves to view of home page"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+    
+    def test_home_page_returns_correct_html(self):
+        """test: home page returns correct html"""
+        request = HttpRequest()
+        response = home_page(request)
+        html: str = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+    
