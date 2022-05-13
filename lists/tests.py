db7161477343d5327f926fda36411f8127b076e8
Сергей Гурков <sergeygurkov@MacBook-Pro-Sergej.local>
2022-05-13 17:10:17 +0300

    [REF] main: use template for homepage

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+import re
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -7,18 +8,9 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
     """Test for toxicity."""
-
-    def test_root_url_resolves_to_home_page_view(self):
-        """test: root url resolves to view of home page"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
     
     def test_home_page_returns_correct_html(self):
         """test: home page returns correct html"""
-        request = HttpRequest()
-        response = home_page(request)
-        html: str = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'home.html')
     
