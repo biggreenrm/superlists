1c0b98e7445485deb4e3307923cb60e8d86f78bb
Сергей Гурков <sergeygurkov@MacBook-Pro-Sergej.local>
2022-05-24 20:17:59 +0300

    [IMP] main: main page html generates from template

--- functional_tests.py
+++ functional_tests.py
@@ -42,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New element did not appeared in table"
         )
 
         # The text field still invites her to add another element.
